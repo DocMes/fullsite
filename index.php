083f45d7e8805ca8d08603ccbfc8f720f9cfc188
DocMes <35721038+DocMes@users.noreply.github.com>
2018-01-25 21:12:09 +0200

    Start

--- index.php
+++ index.php
@@ -4,10 +4,32 @@
 	<meta charset="UTF-8">
 	<title>Айтипрогер</title>
     <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+
 </head>
 <body>
 	<header>
-
+        <div id="logo">
+            <a href="http://fullsite.local" title="На главную">
+                <img src="img/logo.png" title="Айтипрогер" alt="Айти">
+                <span>itProger</span>
+            </a>
+        </div>
+        <div id="about">
+            <a href="" title="Узнать детальнее об рекламу">Реклама</a>
+            <a href="" title="Написать нам письмо">Обратная связь</a>
+        </div>
+        <div id="reg_auth">
+            <a href="" title="Войти в кабинет пользователя">
+                <div id="btn">
+                    Войти
+                </div>
+            </a>
+            <a href="" title="Зарегистрироваться на сайте">
+                <div id="btn">
+                    Регистрация
+                </div>
+            </a>
+        </div>
     </header>
 </body>
 </html>
