2f2728154d4116483a3e0dd88d5e04264150668d
DocMes <35721038+DocMes@users.noreply.github.com>
2018-01-25 20:31:22 +0200

    Start

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Айтипрогер</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+</head>
+<body>
+	<p>тест</p>
+</body>
+</html>
